8085a8ddd2d1469cd2bba03a3ed933486d5c3949
Sam Cook <l.samuel.cook@gmail.com>
2020-02-11 15:19:20 -0500

    Add graph interfaces.

---
+++ src/LiebasamUtils/Collections/IGraph.cs
@@ -0,0 +1,45 @@
+﻿namespace LiebasamUtils.Collections
+{
+    /// <summary>
+    /// Interface for a collection of vertices and edges.
+    /// </summary>
+    public interface IGraph<TVert, TEdge> : IReadOnlyGraph<TVert, TEdge>
+    {
+        /// <summary>
+        /// Access and manipulate the data at a specified vertex.
+        /// </summary>
+        new TVert this[uint id] { get; set; }
+
+        /// <summary>
+        /// Access and manipulate the data at a specified edge.
+        /// </summary>
+        new TEdge this[uint idFrom, uint idTo] { get; set; }
+
+        /// <summary>
+        /// Adds a vertex to the graph, overriding one if it already exists.
+        /// </summary>
+        void AddVertex(uint id, TVert value);
+
+        /// <summary>
+        /// Adds an edge to the graph, overriding one if it already exists.
+        /// </summary>
+        void AddEdge(uint idFrom, uint idTo, TEdge value);
+
+        /// <summary>
+        /// Attempts to remove a vertex with the given ID. This method returns
+        /// false if the vertex does not exist.
+        /// </summary>
+        bool RemoveVertex(uint id);
+
+        /// <summary>
+        /// Attempts to remove an edge with the given IDs. This method returns
+        /// false if the edge does not exist.
+        /// </summary>
+        bool RemoveEdge(uint idFrom, uint idTo);
+
+        /// <summary>
+        /// Removes all vertices and edges from the graph.
+        /// </summary>
+        void Clear();
+    }
+}
---
+++ src/LiebasamUtils/Collections/IReadOnlyGraph.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace LiebasamUtils.Collections
+{
+    /// <summary>
+    /// Read-only interface for a collection of vertices and edges.
+    /// </summary>
+    public interface IReadOnlyGraph<TVert, TEdge>
+    {
+        /// <summary>
+        /// Access the data at a specified vertex.
+        /// </summary>
+        TVert this[uint id] { get; }
+
+        /// <summary>
+        /// Access the data at a specified edge.
+        /// </summary>
+        TEdge this[uint idFrom, uint idTo] { get; }
+
+        /// <summary>
+        /// Number of vertices in the graph.
+        /// </summary>
+        int VertexCount { get; }
+
+        /// <summary>
+        /// Number of edges in the graph.
+        /// </summary>
+        int EdgeCount { get; }
+
+        /// <summary>
+        /// Enumerates the vertices.
+        /// </summary>
+        IEnumerable<Vertex<TVert>> Vertices { get; }
+
+        /// <summary>
+        /// Enumerates the edges.
+        /// </summary>
+        IEnumerable<Edge<TEdge>> Edges { get; }
+
+        /// <summary>
+        /// Returns true if the graph contains the specified vertex.
+        /// </summary>
+        bool ContainsVertex(uint id);
+
+        /// <summary>
+        /// Returns true if the graph contains the specified edge.
+        /// </summary>
+        bool ContainsEdge(uint idFrom, uint idTo);
+
+        /// <summary>
+        /// Returns the data in a given vertex.
+        /// </summary>
+        TVert GetVertex(uint id);
+
+        /// <summary>
+        /// Returns the data in a given edge.
+        /// </summary>
+        TEdge GetEdge(uint idFrom, uint idTo);
+
+        /// <summary>
+        /// Attempts to get the data on a given vertex.
+        /// </summary>
+        /// <returns>True if the vertex exists, false otherwise.</returns>
+        bool TryGetVertex(uint id, out TVert value);
+
+        /// <summary>
+        /// Attempts to get the data on a given edge.
+        /// </summary>
+        /// <returns>True if the edge exists, false otherwise.</returns>
+        bool TryGetEdge(uint idFrom, uint idTo, out TEdge value);
+    }
+}
